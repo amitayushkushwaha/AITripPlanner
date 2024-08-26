0671a8580781313875361c43764b0a7b60ddfc26
amitayushkushwaha <amitkushwaha10122003@gmail.com>
2024-08-26 21:16:14 +0530

    firebase config change

--- src/service/firebaseConfig.js
+++ src/service/firebaseConfig.js
@@ -8,15 +8,14 @@ import {getFirestore} from "firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optionals
 const firebaseConfig = {
-  apiKey: import.meta.env.FIREBASE_API_KEY,
-  authDomain:import.meta.env.FIREBASE_AUTH_DOMIAN ,
-  projectId: import.meta.env.FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.FIREBASE_SENDER_ID,
-  appId:import.meta.env.FIREBASE_APP_ID,
-  measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyBWRsfcIGz2MW7rUaApo6lPDufLUbuP3-g",
+  authDomain: "aitripplanner-3eea5.firebaseapp.com",
+  projectId: "aitripplanner-3eea5",
+  storageBucket: "aitripplanner-3eea5.appspot.com",
+  messagingSenderId: "715912919515",
+  appId: "1:715912919515:web:45999bda2793f9806f4ffe",
+  measurementId: "G-2DCDY4MCNR"
 };
-
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const db=getFirestore(app);
